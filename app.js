0bac5147dba3c478637745f047f49b19962debd7
Josh Curl <josh@curl.me>
2020-02-05 12:01:47 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http')
 const metrics = require('./metrics')
 
 startServer("Server", 3000, (request, response) => {
-  response.end('Hello world! (27)')
+  response.end('Hello world! (28)')
 });
 
 startServer("Metrics Server", 2112, metrics.handler);
